ec376f4e0739f83aaf85cd8c8a1f9d960f8e38ce
luckygxf <403261934@qq.com>
2026-05-31 11:31:22 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home'
+import About from './About'
+
+// function App() {
+//   return (
+//     <h1>Hello world1111</h1>
+//   )
+// }
+
+const App=() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+    </Router>
+  )
+};
+
+
 
 export default App;
---
+++ src/FunDemo.js
@@ -0,0 +1,7 @@
+function FunDemo() {
+    return (
+      <h1>Fun Wolrd</h1>
+    ) 
+  }
+
+  export default FunDemo;
